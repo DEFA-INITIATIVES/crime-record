c3b798bbe35e8f301e6ddba7aad65cc8c9af2aac
engWafula <wafulaallan5@gmail.com>
2023-05-27 18:34:02 +0300

    adding modal

--- src/components/DataTable/Forensictable.js
+++ src/components/DataTable/Forensictable.js
@@ -1,9 +1,14 @@
-import React from "react";
-import Table, { SelectColumnFilter } from "../DataTable/index";
+import { Table } from 'antd';
+import qs from 'qs';
+import React, { useEffect, useState } from 'react';
+import { SelectColumnFilter } from "../DataTable/index";
 import apiClient from "../../api/apiClient";
 import { useDispatch, useSelector } from "react-redux";
 import { useQuery } from "react-query";
 import addData from "../../redux/dataSlice";
+import { Button } from 'antd';
+import MyModal from "../Model"
+
 
 const getData = () => {
 	const data = [
@@ -16,49 +21,80 @@ const getData = () => {
 	];
 	return [...data];
 };
-
-function Forensictable({
-	hName,
-	aName,
-	hDesc,
-	aDesc,
-	hCode,
-	aCode,
-	hSuspect,
-	aSuspect,
-}) {
+const getRandomuserParams = (params) => ({
+	results: params.pagination?.pageSize,
+	page: params.pagination?.current,
+	...params,
+});
+function  Forensictable() {
 	const token = localStorage.getItem("userToken");
 	const dispatch = useDispatch();
+	const [resultData, setResult] = useState()
+	const [crimedata, setData] = useState();
+	const [isOpen,setIsOpen] = useState(false)
+	const [closeModal,setCloseModal] = useState()
+	const [loading, setLoading] = useState(false);
+	const [tableParams, setTableParams] = useState({
+		pagination: {
+			current: 1,
+			pageSize: 10,
+		},
+	});
 
 	const getReports = async () => {
-		const res = await apiClient.get("/forensics", {
+		setLoading(true);
+		const res = await apiClient.get(`/crimes?${qs.stringify(getRandomuserParams(tableParams))}`, {
 			headers: {
 				Authorization: `Bearer ${token}`,
 			},
 		});
+		setData(res.data)
+		setLoading(false);
+		setTableParams({
+			...tableParams,
+			pagination: {
+				...tableParams.pagination,
+				total: res.length,
+			},
+		});
+	};
+	useEffect(() => {
+		getReports()
+	}, [JSON.stringify(tableParams)]);
 
-		return res.data;
+	console.log(crimedata, "<<<<<<")
+
+
+	const handleTableChange = (pagination, filters, sorter) => {
+		setTableParams({
+			pagination,
+			filters,
+			...sorter,
+		});
+
+		if (pagination.pageSize !== tableParams.pagination?.pageSize) {
+			setData([]);
+		}
 	};
 
-	const result = useQuery("crimes", getReports);
-   console.log(result)
-	// if (result) dispatch(addData(result.data[0]));
+	// const result = useQuery("crimes", getReports);
 
-	const { isLoading, isError } = result;
-	console.log(result.data, "hey roland am here ");
+	// // if (result) dispatch(addData(result.data[0]));
 
-	const columns = React.useMemo(
-		() => [
+	// const { isLoading, isError } = result;
+	// // console.log(result.data, "hey roland am here ");
+
+	const columns =  [
 			{
-				Header: "Name",
-				accessor: "name",
+				title: "Name",
+				dataIndex: "name",
 				// Cell: AvatarCell,
 				// imgAccessor: "imgUrl",
 				// emailAccessor: "email",
 			},
 			{
-				Header: "Description",
-				accessor: "description",
+				title: "Description",
+				dataIndex: "description",
 			},
 			// {
 			//   Header: "Status",
@@ -66,33 +102,58 @@ function Forensictable({
 			//   Cell: StatusPill,
 			// },
 			{
-				Header: "Code",
-				accessor: "code",
+				title: "Code",
+				dataIndex: "code",
 			},
 			{
-				Header: "Suspect",
-				accessor: "suspect",
-				Filter: SelectColumnFilter, // new
-				filter: "includes",
+				title: "Suspect",
+				dataIndex: "suspect",
+				// Filter: SelectColumnFilter, // new
+				// filter: "includes",
 			},
-		],
-		[]
-	);
+			{
+				title: "Actions",
+				dataIndex: "_id",
+				render: (_id) => (
+				  <button onClick={()=>setIsOpen(true)} className ="w-20  mt-2 py-2 bg-slate-600 rounded-md hover:bg-indigo-500 relative text-white">Add Report</button>
+				),
+			  },
+		]
+
+
+		const handleEdit = (id) =>{
+			console.log(id)
+		}
 
 	const data = React.useMemo(() => getData(), []);
 
 	return (
 		<div className="min-h-screen bg-gray-100 text-gray-900">
 
-				<main className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 pt-4">
-					<div className="">
-						<h1 className="text-xl font-semibold">  FORENSICS MODULE</h1>
-					</div>
-					<div className="mt-6">
-						<Table columns={columns} data={data} />
-					</div>
-				</main>
-		
+			<main className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 pt-4">
+				<div className="">
+					<h1 className="text-xl font-semibold">FORENSIC MODULES</h1>
+				</div>
+				<div className="mt-6">
+					{/* <Table columns={columns} data={resultData?.data} /> */}
+					<Table
+						columns={columns}
+						rowKey={(record) => record?.login?.uuid}
+						style={{ cursor: 'pointer', color: 'var(--colorIcon)' }}
+						dataSource={crimedata?.data}
+						pagination={tableParams.pagination}
+						loading={loading}
+						onChange={handleTableChange}
+						scroll={{ x: 'max-content' }}
+
+					/>
+				</div>
+			</main>
+               {
+				isOpen &&(
+					<MyModal isOpen={isOpen} setIsOpen={setIsOpen} closeModal={()=>setIsOpen(false)}/>
+				)
+			   }
 		</div>
 	);
 }
